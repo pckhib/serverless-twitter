614e3f4272ac4c858c1de7a3f3fade861ca84b63
Patrick Hieber <patrickhieber92@gmail.com>
2020-04-10 11:21:04 +0200

    (chore) Use variables for Auth0 config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,11 @@
 const apiId = 'h7b3ayx5ta'
+const auth0Domain = 'pckhib.eu.auth0.com'
+const auth0ClientId = 'uFOO1Y8OfU3VH4xIjcwpreDIuskUN7dH'
+
 export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'pckhib.eu.auth0.com', // Auth0 domain
-  clientId: 'uFOO1Y8OfU3VH4xIjcwpreDIuskUN7dH', // Auth0 client id
+  domain: auth0Domain,
+  clientId: auth0ClientId,
   callbackUrl: 'http://localhost:3000/callback',
 }
